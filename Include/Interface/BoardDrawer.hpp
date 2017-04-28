0445bf9fed8ae169f4e89bcd10a148736bf731ec
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2017-04-28 23:18:27 +0100

    Added simple UI

---
+++ Include/Interface/BoardDrawer.hpp
@@ -0,0 +1,34 @@
+#ifndef GOMOKU_INTERFACE_BOARDDRAWER_HPP
+#define GOMOKU_INTERFACE_BOARDDRAWER_HPP
+
+
+#include "Domain/IBoardObserver.hpp"
+
+#include <memory>
+
+#include "Domain/IBoard.hpp"
+
+
+namespace Gomoku
+{
+namespace Interface
+{
+
+
+class BoardDrawer : public Domain::IBoardObserver
+{
+public:
+    BoardDrawer(std::shared_ptr<Domain::IBoard> board_);
+
+    void onStonePutAt(int x, int y) override;
+
+private:
+    std::shared_ptr<Domain::IBoard> board;
+};
+
+
+} // namespace Interface
+} // namespace Gomoku
+
+
+#endif // GOMOKU_INTERFACE_BOARDDRAWER_HPP
