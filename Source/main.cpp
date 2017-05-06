863c07f149d6629e901b667987b8e3ad02cde7bb
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2017-05-06 17:27:26 +0100

    Added Game class

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,26 +1,18 @@
-#include <memory>
+#include <exception>
+#include <iostream>
 
-#include "Application/GameControllerFactory.hpp"
-#include "Application/ArtificialIntelligence/AlphaBetaPlayerFactory.hpp"
-#include "Application/ArtificialIntelligence/MiniMaxPlayerFactory.hpp"
-#include "Domain/Board.hpp"
-#include "Interface/BoardDrawer.hpp"
-#include "Interface/HumanPlayerFactory.hpp"
+#include "Game.hpp"
 
 
 int main()
 {
-    auto board = std::make_shared<Gomoku::Domain::Board>();
-//    auto playerFactory1 = std::make_shared<Gomoku::Interface::HumanPlayerFactory>("Player 1");
-//    auto playerFactory1 = std::make_shared<Gomoku::Application::ArtificialIntelligence::MiniMaxPlayerFactory>();
-    auto playerFactory1 = std::make_shared<Gomoku::Application::ArtificialIntelligence::AlphaBetaPlayerFactory>();
-    auto playerFactory2 = std::make_shared<Gomoku::Interface::HumanPlayerFactory>("Player 2");
-
-    Gomoku::Application::GameControllerFactory gameControllerFactory;
-    auto gameController = gameControllerFactory.create(board, playerFactory1, playerFactory2);
-
-    Gomoku::Interface::BoardDrawer boardDrawer(board);
-
-    gameController->initializeNewGame();
-    gameController->play();
+    try
+    {
+        Gomoku::Game gomokuGame;
+        gomokuGame.play();
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
