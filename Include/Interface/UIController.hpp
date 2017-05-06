86e121b9e43b08812c4cf1f6b23fed32987dc3e7
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2017-05-06 18:14:52 +0100

    Added UIController

---
+++ Include/Interface/UIController.hpp
@@ -0,0 +1,22 @@
+#ifndef GOMOKU_INTERFACE_UICONTROLLER_HPP
+#define GOMOKU_INTERFACE_UICONTROLLER_HPP
+
+
+namespace Gomoku
+{
+namespace Interface
+{
+
+
+class UIController
+{
+public:
+    void run();
+};
+
+
+} // namespace Interface
+} // namespace Gomoku
+
+
+#endif // GOMOKU_INTERFACE_UICONTROLLER_HPP
