b6c5f1d8247c6c9cd08c3fb525f8b65a23324cd2
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2017-05-22 18:57:01 +0100

    Colors added

--- Source/Interface/BoardDrawer.cpp
+++ Source/Interface/BoardDrawer.cpp
@@ -16,31 +16,31 @@ BoardDrawer::BoardDrawer(std::shared_ptr<Domain::IBoard> board_) : board(board_)
 
 void BoardDrawer::onStonePutAt(int x, int y)
 {
-    drawCurrentBoard();
+    drawCurrentBoard(x, y);
 }
 
 
 void BoardDrawer::onBoardCleared()
 {
-    drawCurrentBoard();
+    drawCurrentBoard(-1, -1);
 }
 
 
-void BoardDrawer::drawCurrentBoard()
+void BoardDrawer::drawCurrentBoard(const int lastMoveX, const int lastMoveY)
 {
-    std::cout << "                         1 1 1 1 1" << std::endl;
-    std::cout << "     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4" << std::endl;
+    std::cout << colorNumbers() << "                         1 1 1 1 1" << std::endl;
+    std::cout << colorNumbers() << "     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4" << std::endl;
 
-    std::cout << "   ,-";
+    std::cout << colorBoard() << "   ,-";
     for(int i = 0; i < board->getSize(); ++i)
         std::cout << "--";
-    std::cout << "," << std::endl;
+    std::cout << "," << colorDefault() << std::endl;
 
     for(int i = 0; i < board->getSize(); ++i)
     {
         if(i < 10)
             std::cout << " ";
-        std::cout << i << " | ";
+        std::cout << colorNumbers() << i << colorBoard() << " | ";
 
         for(int j = 0; j < board->getSize(); ++j)
         {
@@ -52,17 +52,69 @@ void BoardDrawer::drawCurrentBoard()
 
             const auto stone = board->getStone(i, j);
             if(stone == Domain::Stone::Black)
-                std::cout << "x ";
+            {
+                if((i == lastMoveX) && (j == lastMoveY))
+                    std::cout << colorLastMoveBlackStone() << "x " << colorDefault();
+                else
+                    std::cout << colorBlackStone() << "x ";
+            }
             else
-                std::cout << "o ";
+            {
+                if((i == lastMoveX) && (j == lastMoveY))
+                    std::cout << colorLastMoveWhiteStone() << "o " << colorDefault();
+                else
+                    std::cout << colorWhiteStone() << "o ";
+            }
         }
-        std::cout << "|" << std::endl;
+        std::cout << colorBoard() << "|" << std::endl;
     }
 
-    std::cout << "   '-";
+    std::cout << colorBoard() << "   '-";
     for(int i = 0; i < board->getSize(); ++i)
         std::cout << "--";
-    std::cout << "'" << std::endl;
+    std::cout << "'" << colorDefault() << std::endl;
+}
+
+
+std::string BoardDrawer::colorNumbers() const
+{
+    return "\033[34m";
+}
+
+
+std::string BoardDrawer::colorBoard() const
+{
+    return "\033[32m";
+}
+
+
+std::string BoardDrawer::colorBlackStone() const
+{
+    return "\033[31m";
+}
+
+
+std::string BoardDrawer::colorWhiteStone() const
+{
+    return "\033[36m";
+}
+
+
+std::string BoardDrawer::colorLastMoveBlackStone() const
+{
+    return "\033[1;31m";
+}
+
+
+std::string BoardDrawer::colorLastMoveWhiteStone() const
+{
+    return "\033[1;36m";
+}
+
+
+std::string BoardDrawer::colorDefault() const
+{
+    return "\033[0m";
 }
 
 
